503bc4e288035b9deed0fa5cb70f62a293b5c7b5
Yoann <yoann.menard@epitech.eu>
2020-05-13 22:40:06 +0200

    formulaire de connexion renvoie sur le tchat lors de la creation du compte

--- index.js
+++ index.js
@@ -3,21 +3,35 @@ let http = require("http").createServer(app);
 let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/tchat', (req, res) => {
+  res.sendFile(__dirname + '/views/tchat.html');
 });
 
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  let a = socket.broadcast.emit('bonjour');
+
+  io.on('connection'), (socket) => {
+    socket.on('message de tchat', (msg) => {
+      io.emmit('message de tchat', (msg))
+    });
+  }
+  
+  // socket.broadcast.emit('Bonjour');
+
 
   socket.on('chat message', (msg) => {
     console.log('message ' + msg);
   })
+
   socket.on('disconnect', () => {
   console.log('user disconnected');
   });
 });
-  
+
 let PORT = process.env.PORT || 1337;
 
 http.listen(PORT, () => {
