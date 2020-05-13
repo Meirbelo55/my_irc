a1211e9a9b6c5faa7bdc8499ef72190e817a5843
meir <meir.belaich@epitech.eu>
2020-05-13 22:39:52 +0200

    d

--- js/server.js
+++ js/server.js
@@ -1,10 +1,16 @@
 var http = require('http')
-const express = require ('express');
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+var server = http.createServer(app);
+var io = require ('socket.io').listen(server);
 const router = express.Router();
 router.get('/',function(req,res){
     res.sendFile(path.join('/home/meir/Downloads/first-year/my_irc' + '/views/index.html'))
 });
+io.on('connexion',(socket) => {
+    console.log('un user est connecter');
+})
+//console.log(io);
 app.use('/',router);
 app.listen(1337);
